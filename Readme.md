0dd8d5f50aad0f333c1def751c6e70f081dc93e5
Jeff Dyck <Jeff.C.Dyck@gmail.com>
2013-01-08 18:04:08 -0500

    Some cleanup, added new Readme.md, moved test inputs

---
+++ Readme.md
@@ -0,0 +1,51 @@
+Conway's Game of Life
+=====================
+
+A graphical implementation of Conway's Game of Life, using X. 
+
+How to play
+-----------
+Commands:
+      
+new n    -     Creates a new nxn grid and opens a 500x500 window
+
+init     -     Enters initialization mode
+               Takes sets of two numbers x and y and turns that cell on
+               Stops when -1 -1 is read
+               Ex: init 0 0 1 1 2 2 -1 -1
+               Turns on cells (0,0) (1,1) (2,2) and exits initialization mode
+
+print    -     Prints the current grid in the terminal window
+
+step     -     Runs one tick of the grid
+                     Updates the graphical window
+
+steps n  -     Runs n ticks of the graphical window
+
+random n -     Randomly turns n% of the grid on and (100-n)% off
+
+auto n   -     Automatically steps every n milliseconds
+               Will run infinitely. Only way to stop is to Ctrl-c or end the process
+
+Compiling and running
+---------------------
+
+To compile and run: 
+      <blockquote>
+      <p>$ make</p>
+      <p>$ ./life-graphical</p>
+      <p></blockquote>
+
+Common running procedure:
+      <blockquote>
+      <p>./life-graphical</p>
+         <p>new x</p>
+         <p>random y</p>
+         <p>auto z</p>
+      </blockquote>
+
+
+Notes
+-----
+
+This program leaks memory due to the implementation of the Xwindow class.
---
+++ conway.cc
@@ -0,0 +1,72 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  a4q3.cc
+ *
+ *    Description:  Runs Conway's Game of Life
+ *                  Takes user input to create a grid of n cells and runs the simulation
+ *                  with the grid. 
+ *
+ *         Author:  Jeff Dyck
+ *
+ * =====================================================================================
+ */
+
+
+#include <iostream>
+#include "cell.h"
+#include "grid.h"
+
+using namespace std;
+
+int main() {
+   int n;
+   int x = 0;
+   int y = 0;
+   string cmd;
+
+   Grid *grd = new Grid();
+
+   while (cin >> cmd) {
+
+      if (cmd == "new") {
+         cin >> n;
+         grd->init(n);
+
+      } else if (cmd == "init") {
+         cin >> x >> y;
+         while (x != -1 && y != -1) {
+            grd->turnOn(x, y);   
+            cin >> x >> y;
+         }
+
+      } else if (cmd == "print") {
+         cout << *grd;
+
+      } else if (cmd == "step" ) {
+         grd->tick();
+
+      } else if (cmd == "steps") {
+         cin >> n;
+         for (int i=0; i < n; i++) {
+            grd->tick();
+         }
+
+      } else if (cmd == "random") {
+         cin >> n;
+         grd->randomCells(n);
+
+      } else if (cmd == "auto" ) {
+         cin >> n;
+         grd->autoTick(n);
+
+      } else {
+         cout << "Unrecognized command" << endl;
+      }
+
+   }  //while
+
+   delete grd; 
+   
+   return 0;
+}
